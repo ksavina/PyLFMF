a5a8246c10b7678190f284b28fb7d780e8387558
ksavina <60853656+ksavina@users.noreply.github.com>
2021-02-14 11:11:29 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11 import get_cmake_dir
+
+import sys
+
+__version__ = "1.0"
+
+# The main interface is through Pybind11Extension.
+# * You can add cxx_std=11/14/17, and then build_ext can be removed.
+# * You can set include_pybind11=false to add the include directory yourself,
+#   say from a submodule.
+#
+# Note:
+#   Sort input source files if you glob sources to ensure bit-for-bit
+#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+
+ext_modules = [
+    Pybind11Extension("PyLFMF",
+        ["src/main.cpp", "src/Airy.cpp", "src/FlatEarthCurveCorrection.cpp",
+        "src/LFMF.cpp", "src/ResidueSeries.cpp", "src/ValidateInputs.cpp",
+        "src/WiRoot.cpp", "src/werf.cpp"]
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
+]
+
+setup(
+    name="PyLFMF",
+    version=__version__,
+    author="Kirill Savina & Daniel Sherstnev",
+    author_email="ksavina@nes.ru",
+    url="https://github.com/ksavina/PyLFMF/",
+    description="A wrapper for NTIA/LFMF's groundwave propagarion tool",
+    long_description="",
+    ext_modules=ext_modules,
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
+)
